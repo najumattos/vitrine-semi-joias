85a947834748b5ae06ceefc2a3c7d5a9d19722a9
Ana Julia Reis de Mattos <anajuliamattos02@gmail.com>
2026-05-08 13:17:47 -0300

    :sparkles: feat: Add ProductModel

---
+++ Enums/CategoryEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace VitrineSemiJoias.Enums;
+
+public enum CategoryEnum
+{
+    Brinco = 1,
+    Colar = 2,
+    Pulseira = 3,
+    Anel = 4,
+    Pingente = 5,
+    Tornoseleira = 6,
+    Gargantilha = 7,
+    Alianca = 8    
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using VitrineSemiJoias.Enums;
+
+namespace VitrineSemiJoias.Models;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public CategoryEnum CategoryEnum { get; set; }
+    public string ImageUrl { get; set; } = string.Empty;
+    public bool IsAvailable { get; set; }
+    public int StockQuantity { get; set; }
+}

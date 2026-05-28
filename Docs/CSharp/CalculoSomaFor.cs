fe84bf3488208fc449c8d5a1444d95b43069d4bd
Ana Julia Reis de Mattos <anajuliamattos02@gmail.com>
2026-05-27 22:33:55 -0300

    :alien: Csharp

---
+++ Docs/CSharp/CalculoSomaFor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FDEVS;
+
+public class CalculoSomaFor
+{
+    public void CalcularSomaFor()
+    {
+        Console.WriteLine("Calcular Soma do For");
+        int soma = 0;
+
+        // O laço começa em 1, executa enquanto for menor ou igual a 10, e avança de 1 em 1
+        for (int i = 1; i <= 10; i++)
+        {
+            soma += i; // Adiciona o valor atual de 'i' ao total acumulado
+        }
+        // Exibe o resultado final no console
+        Console.WriteLine($"A soma dos números de 1 a 10 é: {soma}");
+    }
+}

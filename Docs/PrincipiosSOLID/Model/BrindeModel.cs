91ca7b60dd8a49d916550c9fa414f73ea10e7f85
Ana Julia Reis de Mattos <anajuliamattos02@gmail.com>
2026-05-28 16:13:18 -0300

    :truck: Alterando Docs

--- Docs/PrincipiosSOLID/Model/BrindeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PrincipiosSOLID.Model;
-
-public class BrindeModel : ProdutoModel      
-{
-    public int IdBrinde { get; set; }
-    public string TipoBrinde { get; set; }
-    public DateTime DataValidade { get; set; }
-    //outras props
-}
